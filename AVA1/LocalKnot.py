969cf8b679f071ba0117a14ec92132a9092ec137
Gilles Baatz <baatzgilles@gmail.com>
2013-10-16 21:06:28 +0200

    AVA1 open Port, choose neighbours

--- AVA1/LocalKnot.py
+++ AVA1/LocalKnot.py
@@ -1,5 +1,7 @@
 import json
 from pprint import pprint
+import random
+import socket
 
 __author__ = 'me'
 
@@ -10,15 +12,19 @@ import os
 class LocalKnot(Process):
     def __init__(self, ID):
         super(LocalKnot, self).__init__()
-        self.__ID = ID
+        self.__ID = str(ID)
         self.__ips_and_ports = None
+        self.__ip = None
+        self.__port = None
+        self.__listeningSocket = None
+        self.__neighbours = {}
 
     def run(self):
         self.info()
         self.read_input_file()
-        '''
         self.open_port()
         self.choose_neighbours()
+        '''
         init = True
         while True:
             message = self.receive_message()
@@ -27,6 +33,7 @@ class LocalKnot(Process):
                 self.send_ID_to_neighbours()
                 init = False
                 '''
+
 
     def info(self):
         print 'module name:', __name__
@@ -39,5 +46,22 @@ class LocalKnot(Process):
     def read_input_file(self):
         json_data = open('AVA1/json_data')
         self.__ips_and_ports = json.load(json_data)
-        print self.__ips_and_ports
         json_data.close()
+
+    def open_port(self):
+        self.__ip = self.__ips_and_ports[self.__ID]["ip"]
+        self.__port = self.__ips_and_ports[self.__ID]["port"]
+        del self.__ips_and_ports[self.__ID]
+
+        self.__listeningSocket = socket.socket()
+        host = socket.gethostname()
+        self.__listeningSocket.bind((host, self.__port))
+
+    def choose_neighbours(self):
+        for i in range(3):
+            random_index = random.randint(0, len(self.__ips_and_ports)-1)
+            key = self.__ips_and_ports.keys()[random_index]
+            self.__neighbours[key] = self.__ips_and_ports[key]
+            del self.__ips_and_ports[key]
+
+
--- AVA1/Watchman.py
+++ AVA1/Watchman.py
@@ -3,7 +3,7 @@ from LocalKnot import LocalKnot
 __author__ = 'me'
 
 if __name__ == '__main__':
-    for num in range(7):
+    for num in range(8):
         l = LocalKnot(num)
         l.start()
         l.join()
