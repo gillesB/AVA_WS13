325b1da52ec3e33dc3d3462b2e5092ba1ed44299
Gilles Baatz <baatzgilles@gmail.com>
2013-10-21 21:30:38 +0200

    changes in Watchman and json_data

--- AVA1/Watchman.py
+++ AVA1/Watchman.py
@@ -12,16 +12,16 @@ __author__ = 'me'
 def send_init_to_zero():
     sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostname()
-    sender.connect((host, 50000))
+    sender.connect((host, 50001))
     init_message = Message('init', 'War... War never changes', True)
     sender.sendall(cPickle.dumps(init_message))
 
 
 if __name__ == '__main__':
-    for num in range(4):
+    for num in range(7):
         print 'Watchman starts', num
         l = LocalKnot(num+1)
         l.start()
         #l.join()
     time.sleep(1)
-    #send_init_to_zero()
+    send_init_to_zero()
