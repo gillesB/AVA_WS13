d8798e2be2c185f464da1c6b5f4592a9dc17cca1
Gilles Baatz <baatzgilles@gmail.com>
2013-12-11 21:42:22 +0100

    added Sender to Message

--- CodeBase/Message.py
+++ CodeBase/Message.py
@@ -7,20 +7,27 @@ class Message():
     danach wieder deserialisiert werden kann.
     '''
 
-    def __init__(self, action, message="", control=False):
+    def __init__(self, action, message="", control=False, sender=None):
         self.__control = control # i.d.R ein boolean der angibt, ob es sich um eine Kontrollnachricht handelt,
         #sprich ob die Nachricht von einem Beobachter stammt (True = Nachricht kommt von einem Beobachter).
         self.__message = message # die eigentlichen Daten die verwendet werden sollen
         self.__action = action # eine Aktion, auf die die Knoten reagieren koennen, z.B. um die empfangen Daten zu verarbeiten
+        self.__sender = sender # der Absender der Nachricht
 
     def printToString(self):
-        control = ""
+        return_str = ""
         if self.__control:
-            control = '(Kontrollnachricht)'
-        return control + ' Aktion: ' + str(self.__action) + ' Nachricht: ' + str(self.__message)
+            control = '(Kontrollnachricht) '
+        return_str = return_str + 'Aktion: ' + str(self.__action) + ' Inhalt: ' + str(self.__message)
+        if self.__sender:
+            return_str = return_str + ' Sender: ' + str(self.__sender)
+        return return_str
 
     def getAction(self):
         return self.__action
 
     def getMessage(self):
         return self.__message
+
+    def getSender(self):
+        return self.__sender
