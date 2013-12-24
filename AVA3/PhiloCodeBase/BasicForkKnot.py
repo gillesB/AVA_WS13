fff362dfc18ad290cc475011f3a9984e1fa67f26
Gilles Baatz <baatzgilles@gmail.com>
2013-12-24 16:22:47 +0100

    Fixed bugs in BasicForkKnot

--- AVA3/PhiloCodeBase/BasicForkKnot.py
+++ AVA3/PhiloCodeBase/BasicForkKnot.py
@@ -24,17 +24,23 @@ class BasicForkKnot(AbstractGraphKnot):
     def process_received_message(self, connection, message):
         if message.getAction() == "orderFork":
             self.ordered_by = message.getSender()
+            self.logger.info("I was ordered by " + message.getSender())
         elif message.getAction() == "returnFork":
             self.set_free(message.getSender())
 
     def give_philosopher_a_fork(self):
-        if self.ordered_by >= 0:
+        if self.free and self.ordered_by >= 0:
             philosopher_receives_fork_message = Message("receiveFork", "A fork for a philiospher.", sender=self._ID)
             self.send_message_to_id(philosopher_receives_fork_message, self.ordered_by)
+            self.owner = self.ordered_by
+            self.free = False
             self.ordered_by = -1
+            self.logger.info("My new owner is " + self.owner)
 
     def set_free(self, request_comes_from_id):
         if request_comes_from_id == self.owner:
             self.free = True
+            self.owner = None
+            self.logger.info("I have now owner.")
 
 
