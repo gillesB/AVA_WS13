629428ab245a8fecbe0a01ba6c609690fde00f9c
Gilles Baatz <baatzgilles@gmail.com>
2013-12-26 17:57:35 +0100

    EdgeChasing seems to work

--- AVA3/AVA3b/EdgeChasingForkKnot.py
+++ AVA3/AVA3b/EdgeChasingForkKnot.py
@@ -11,11 +11,15 @@ class EdgeChasingForkKnot(BasicForkKnot):
     def process_received_message(self, connection, message):
         super(EdgeChasingForkKnot, self).process_received_message(connection, message)
         if message.getAction() == "isDeadlock":
-            if self.ordered_by:
-                self.logger.info("Sending isDeadlock message to my owner, to ask if he is waiting for another fork.")
-                self.send_message_to_id(message, self.owner)
+            if self._ID not in message.getMessage():
+                if self.ordered_by:
+                    self.logger.info("Sending isDeadlock message to my owner, to ask if he is waiting for another fork.")
+                    message.getMessage().append(self._ID)
+                    self.send_message_to_id(message, self.owner)
+                else:
+                    self.logger.info("Having no owner. No deadlock was found.")
             else:
-                self.logger.info("Having no owner. No deadlock was found.")
+                self.logger.info("I already appear in the message. No deadlock was found.")
 
 
 
--- AVA3/AVA3b/EdgeChasingPhiloKnot.py
+++ AVA3/AVA3b/EdgeChasingPhiloKnot.py
@@ -1,3 +1,5 @@
+from dns import message
+from sets import Set
 import time
 from datetime import datetime
 from AVA3.PhiloCodeBase.BasicPhilosopherKnot import BasicPhilosopherKnot
@@ -10,6 +12,7 @@ class EdgeChasingPhiloKnot(BasicPhilosopherKnot):
 
     def __init__(self, ID, connections_filename, topology_filename):
         super(EdgeChasingPhiloKnot, self).__init__(ID, connections_filename, topology_filename)
+        self.requested_fork_at_possible_deadlock = None
 
     def run(self):
         self.info()
@@ -24,15 +27,15 @@ class EdgeChasingPhiloKnot(BasicPhilosopherKnot):
             self.eat()
             self.return_forks()
 
-    def think(self):
-        time_to_think = 2  # [s = ms / 1000]
-        self.logger.info("I am thinking now for " + str(time_to_think) + " seconds.")
-        time.sleep(time_to_think)
-
-    def eat(self):
-        time_to_eat = 2  # [s = ms / 1000]
-        self.logger.info("I am eating now for " + str(time_to_eat) + " seconds.")
-        time.sleep(time_to_eat)
+    # def think(self):
+    #     time_to_think = 2  # [s = ms / 1000]
+    #     self.logger.info("I am thinking now for " + str(time_to_think) + " seconds.")
+    #     time.sleep(time_to_think)
+    #
+    # def eat(self):
+    #     time_to_eat = 2  # [s = ms / 1000]
+    #     self.logger.info("I am eating now for " + str(time_to_eat) + " seconds.")
+    #     time.sleep(time_to_eat)
 
     @staticmethod
     def wait_till_full_second():
@@ -42,20 +45,34 @@ class EdgeChasingPhiloKnot(BasicPhilosopherKnot):
     def process_received_message(self, connection, message):
         super(EdgeChasingPhiloKnot, self).process_received_message(connection, message)
         if message.getAction() == "isDeadlock":
-            if message.getMessage() == self._ID:
-                self.logger.info("A deadlock was found.")
-                self.resolve_deadlock()
-            elif self.waiting_for:
-                self.logger.info("Sending isDeadlock message to requested fork.")
-                self.send_message_to_id(message, self.waiting_for)
+            if self.waiting_for and message.getMessage()[-1] != self.requested_fork_at_possible_deadlock and (message.getMessage()[0] == self._ID or self._ID not in message.getMessage()):
+            #possible deadlock
+                if message.getMessage()[0] == self._ID:
+                    #is deadlock
+                    self.logger.info("A deadlock was found.")
+                    self.resolve_deadlock()
+                else:
+                    #might be a deadlock
+                    self.logger.info("Sending isDeadlock message to requested fork.")
+                    message.getMessage().append(self._ID)
+                    self.send_message_to_id(message, self.waiting_for)
             else:
-                self.logger.info("Not waiting for a fork. No deadlock was found.")
+                #no deadlock
+                if not self.waiting_for:
+                    self.logger.info("Not waiting for a fork. No deadlock was found.")
+                elif message.getMessage()[-1] == self.requested_fork_at_possible_deadlock:
+                    self.logger.info("Received the isDeadlock message from requested fork. No deadlock was found.")
+                elif self._ID in message.getMessage():
+                    self.logger.info("I already am in the list of the isDeadlock message. No deadlock was found.")
+                else:
+                    self.logger.info("Strange another case appeared. No deadlock was found.")
         elif message.getAction() == "checkDeadlock":
-            is_deadlock_message = Message("isDeadlock", self._ID, True, self._ID)
+            is_deadlock_message = Message("isDeadlock", list(self._ID), True, self._ID)
             if not self.waiting_for:
                 self.logger.info("Having both forks. No deadlock.")
             else:
                 self.logger.info("Sending isDeadlock message to requested fork.")
+                self.requested_fork_at_possible_deadlock = self.waiting_for
                 self.send_message_to_id(is_deadlock_message, self.waiting_for)
 
     def resolve_deadlock(self):
